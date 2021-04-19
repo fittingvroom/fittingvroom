1c31b3d6d110abb27f2bcc568b0cb1a8829176e9
Alexander Ermakov <ermakov.a.a@mail.ru>
2021-04-19 15:29:20 +0300

    фрагмент и ресурсы

--- app/src/main/java/com/fittingvroom/ui/model/ModelFragment.kt
+++ app/src/main/java/com/fittingvroom/ui/model/ModelFragment.kt
@@ -4,28 +4,61 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.fittingvroom.R
+import com.fittingvroom.databinding.FragmentModelBinding
 
 class ModelFragment : Fragment() {
 
     private lateinit var modelViewModel: ModelViewModel
+    private var viewBinding: FragmentModelBinding? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        initViewModel()
+        viewBinding = FragmentModelBinding.inflate(inflater, container, false)
+        return viewBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolbarNavigation()
+        setBtnListeners()
+    }
+
+    private fun initViewModel() {
         modelViewModel =
                 ViewModelProvider(this).get(ModelViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_model, container, false)
-        val textView: TextView = root.findViewById(R.id.text_model)
-        modelViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    }
+
+    private fun initToolbarNavigation() {
+        val binding = viewBinding ?: return
+        binding.modelToolbar.setNavigationIcon(R.drawable.ic_toolbar_back_btn)
+        binding.modelToolbar.setNavigationOnClickListener {
+            Toast.makeText(requireContext(), "Возврат к предыдущему фрагменту", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setBtnListeners() {
+        val binding = viewBinding ?: return
+        binding.modelBottomButton.setOnClickListener {
+            Toast.makeText(requireContext(), "К окну добавления параметров", Toast.LENGTH_SHORT).show()
+        }
+        binding.mannequinHelpImageBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "К окну помощи", Toast.LENGTH_SHORT).show()
+        }
+        binding.mannequinShareImageBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "Поделиться", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewBinding = null
     }
 }
