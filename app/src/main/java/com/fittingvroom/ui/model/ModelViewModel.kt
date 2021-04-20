0efe4ef82221b3886448e567db1713525ced0507
Alexander Ermakov <ermakov.a.a@mail.ru>
2021-04-20 21:18:47 +0300

    функции для навигации в модели

--- app/src/main/java/com/fittingvroom/ui/model/ModelViewModel.kt
+++ app/src/main/java/com/fittingvroom/ui/model/ModelViewModel.kt
@@ -1,13 +1,11 @@
 package com.fittingvroom.ui.model
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ModelViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is model Fragment"
-    }
-    val text: LiveData<String> = _text
+    fun shareClick() {}
+    fun toHelpScreen() {}
+    fun toParametersScreen() {}
+    fun backClick() {}
 }
