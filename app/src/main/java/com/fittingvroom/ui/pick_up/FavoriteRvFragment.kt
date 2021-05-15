1c76096e9681f241ffd1c895794de48ebb7c9992
Alexander Ermakov <ermakov.a.a@mail.ru>
2021-05-15 22:24:22 +0300

    рефакторинг

--- app/src/main/java/com/fittingvroom/ui/pick_up/FavoriteRvFragment.kt
+++ app/src/main/java/com/fittingvroom/ui/pick_up/FavoriteRvFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.fittingvroom.R
 import com.fittingvroom.databinding.FragmentPickUpRvBinding
@@ -67,7 +68,7 @@ class FavoriteRvFragment : Fragment() {
     }
 
     private fun setupObservers() {
-        viewModel.getFavorites().observe(viewLifecycleOwner, { it ->
+        viewModel.getFavorites().observe(viewLifecycleOwner, Observer { it ->
             it?.let { result ->
                 when (result) {
                     is AppState.Success -> {
