69de3d4523423e111a7f4c7cef010a2a179b16bc
shindje <aleks.shindje@gmail.com>
2021-05-18 18:57:06 +0300

    Загрузка другой модели, если выбран товар Кнопки Избранное, Корзина на экране примерки

--- app/src/main/java/com/fittingvroom/ui/fitting/FittingFragment.kt
+++ app/src/main/java/com/fittingvroom/ui/fitting/FittingFragment.kt
@@ -12,13 +12,16 @@ import com.fittingvroom.data.ModelParametersData
 import com.fittingvroom.databinding.FragmentFittingBinding
 import com.fittingvroom.model.AppState
 import com.fittingvroom.ui.base.BaseFragment
+import com.fittingvroom.ui.pick_up.PickUpRvViewModel
 import com.fittingvroom.ui.pick_up.ProductCardFragment
 import com.fittingvroom.ui.view3d.SceneViewer
+import com.google.android.material.snackbar.Snackbar
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class FittingFragment : BaseFragment<AppState<ModelParametersData>>() {
 
     override lateinit var model: FittingViewModel
+    lateinit var productCardModel: PickUpRvViewModel
     private var viewBinding: FragmentFittingBinding? = null
     private val navigation by lazy { findNavController() }
     private var productId: Int? = null
@@ -48,11 +51,30 @@ class FittingFragment : BaseFragment<AppState<ModelParametersData>>() {
         productSize = arguments?.getString(ProductCardFragment.SIZE_PRODICT) ?: ""
         if (productId == null || productSize.isNullOrEmpty()) {
             viewBinding?.fittingFavoriteImageBtn?.visibility = View.GONE
-            viewBinding?.fittingCartAddBtn?.visibility = View.GONE
+            viewBinding?.fittingFavoriteImageOnBtn?.visibility = View.GONE
+            viewBinding?.fittingBasketBtn?.visibility = View.GONE
+        } else {
+            productCardModel.getFavorites(productId!!).observe(viewLifecycleOwner, { isFavorite ->
+                showFavorite(isFavorite)
+            })
+        }
+    }
+
+    private fun showFavorite(isFavorite: Boolean) {
+        val binding = viewBinding ?: return
+        if (isFavorite) {
+            binding.fittingFavoriteImageBtn.visibility = View.GONE
+            binding.fittingFavoriteImageOnBtn.visibility = View.VISIBLE
+        } else {
+            binding.fittingFavoriteImageBtn.visibility = View.VISIBLE
+            binding.fittingFavoriteImageOnBtn.visibility = View.GONE
         }
     }
 
     private fun initViewModel() {
+        val pickUpRvViewModel: PickUpRvViewModel by viewModel()
+        productCardModel = pickUpRvViewModel
+
         val viewModel: FittingViewModel by viewModel()
         model = viewModel
         model.subscribe().observe(viewLifecycleOwner, Observer {renderData(it) })
@@ -79,6 +101,24 @@ class FittingFragment : BaseFragment<AppState<ModelParametersData>>() {
         }
         binding.fittingFavoriteImageBtn.setOnClickListener {
 
+        }
+        binding.fittingBasketBtn.setOnClickListener {
+            productCardModel.setBasket(productId!!, productSize!!)
+            Snackbar.make(
+                    binding.fittingBasketBtn,
+                    R.string.added_to_basket,
+                    Snackbar.LENGTH_SHORT
+            )
+                    .show()
+        }
+        binding.fittingFavoriteImageBtn.setOnClickListener {
+            productCardModel.setFavorites(productId!!)
+            showFavorite(true)
+
+        }
+        binding.fittingFavoriteImageOnBtn.setOnClickListener {
+            productCardModel.setFavorites(productId!!)
+            showFavorite(false)
         }
     }
 
@@ -108,7 +148,8 @@ class FittingFragment : BaseFragment<AppState<ModelParametersData>>() {
                         requireContext(),
                         resources,
                         viewBinding?.fittingSceneView,
-                        viewBinding?.fittingSceneViewPb
+                        viewBinding?.fittingSceneViewPb,
+                        !(productId == null || productSize.isNullOrEmpty())
                     )
                 } else {
                     showImageView()
